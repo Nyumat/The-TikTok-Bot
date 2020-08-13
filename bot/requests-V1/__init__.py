f425b422d5e7b61a44a4faddafc71ea116ae607b
thomas <46255836+Nyumat@users.noreply.github.com>
2020-08-13 11:16:52 -0400

    Update __init__.py

--- bot/requests-V1/__init__.py
+++ bot/requests-V1/__init__.py
@@ -1,7 +1,16 @@
+## Imports all the modules for the script.
+
 from os.path import dirname, basename, isfile, join
 import glob
 modules = glob.glob(join(dirname(__file__), "*.py"))
 __all__ = [ basename(f)[:-3] for f in modules if isfile(f) and not f.endswith('__init__.py')]
 
+## Calls program files
+
 def main:
-  
+  if __name__ == '__init__':
+    os.system('cls && title [The TikTok Bot]')
+    main = TikTok()
+    main.scrapeProxyList(1 || 2)
+    main.start()
+main()
