f5e1a81846de52ba6bcedb0d86b6c432401090f0
thomas <46255836+Nyumat@users.noreply.github.com>
2020-08-13 10:11:45 -0400

    Update setup.py

--- setup/setup.py
+++ setup/setup.py
@@ -1,2 +1,22 @@
+import os
 from os import system
 
+def installLibraries():
+    
+   print(f'Installing Libraries....')
+   os.wait()  
+  
+   system('pip install requests')
+   system('pip install threading)
+   system('pip install bs4')
+   system('pip install time')
+   system('pip install math')
+   system('pip install selenium')
+   system('pip install Keys')
+   system('pip install ActionsChains')
+   system('pip install pygame')
+          
+installLibraries()
+          
+print('\n'+'Required Libraries are now installed.')
+system('pause >NUL')
