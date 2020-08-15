6000d4b673d2a4234499fb10947021faa069dcbe
thomas <46255836+Nyumat@users.noreply.github.com>
2020-08-14 22:10:30 -0400

    Update __init__.py

--- bot/requests-V1/__init__.py
+++ bot/requests-V1/__init__.py
@@ -1,7 +1,9 @@
-## Imports all the modules for the script.
 
+## Imports all the modules for the script.
 from os.path import dirname, basename, isfile, join
 import glob
+import os 
+
 modules = glob.glob(join(dirname(__file__), "*.py"))
 __all__ = [ basename(f)[:-3] for f in modules if isfile(f) and not f.endswith('__init__.py')]
 
