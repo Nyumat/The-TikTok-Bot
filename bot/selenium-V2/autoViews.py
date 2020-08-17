12206306b02d4f062468058163f181c5024f6a06
um <46255836+Nyumat@users.noreply.github.com>
2020-08-17 14:57:21 -0400

    Added user specified view sending amounts

--- bot/selenium-V2/autoViews.py
+++ bot/selenium-V2/autoViews.py
@@ -2,22 +2,25 @@ from selenium import webdriver
 from selenium.webdriver.support.ui import Select
 import time
 
+# Links that through getURL will be referenced
 mainURL = "https://fireliker.com/"
 panelURL = "https://fireliker.com/welcome.php"
 autoviewURL = "https://fireliker.com/autoviews.php"
 loop = False
 
+# Prompt the user for their information and desired views.
 print("What is your TikTok user name?")
 user = input("-->>  ")
 
 print("How many views would you like?")
+print("Options are : [1000] [800] [600] [400] [200] ")
 views = input("")
 
 chrome_options = webdriver.ChromeOptions()
 chrome_options.add_argument("--log-level=3")
 chrome_options.add_argument("--headless")
 driver = webdriver.Chrome()
-driver.set_window_size(960, 540)
+driver.set_window_size(500, 540)
 driver.get(mainURL)
 time.sleep(2)
 loginInput = driver.find_element_by_name("username")
@@ -32,7 +35,21 @@ auto = driver.find_element_by_xpath("/html/body/div[2]/div[2]/div[1]/fieldset/di
 
 driver.get(autoviewURL)
 select = Select(driver.find_element_by_id('select')) 
-select.select_by_value('5')
+# Select Specified  Views logic
+if views == "1000":
+    select.select_by_value('5')
+elif views == "800":
+    select.select_by_value('4')
+elif views == "600":
+    select.select_by_value('3')
+elif views == "400":
+    select.select_by_value('2')
+elif views == "200":
+    select.select_by_value('1')
+else:
+    # User didn't enter one of the dropdown values
+    print("You  did not specify a number of views within  range, please re-run the program.")
+    breakpoint()
 select_button = driver.find_element_by_xpath("/html/body/div[2]/div/div/div[1]/div/div/div[2]/div/form/button").click()
 
 # Code below isn't needed unless you're in a headless browser.
@@ -43,6 +60,7 @@ select_button = driver.find_element_by_xpath("/html/body/div[2]/div/div/div[1]/d
 #send_views = driver.find_element_by_xpath("/html/body/div[2]/div/div/div[1]/div/div/div[2]/div/form/button")
 #time.sleep(2)
 
+# Wait 5 mins to run again.
 time.sleep(315)
 loop = True
 
