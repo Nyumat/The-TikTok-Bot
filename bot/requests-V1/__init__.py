5b17c0a8d5dc6b37c6c833ee6426bad0a02ae20f
thomas <46255836+Nyumat@users.noreply.github.com>
2020-09-04 12:55:08 -0400

    --Typos

--- bot/requests-V1/__init__.py
+++ bot/requests-V1/__init__.py
@@ -1,5 +1,5 @@
 
-## Imports all the modules for the script.
+# Imports all the modules for the script.
 from os.path import dirname, basename, isfile, join
 import glob
 import os 
@@ -7,9 +7,8 @@ import os
 modules = glob.glob(join(dirname(__file__), "*.py"))
 __all__ = [ basename(f)[:-3] for f in modules if isfile(f) and not f.endswith('__init__.py')]
 
-## Calls program files
-
-def main:
+# Calls program files
+def main():
   if __name__ == '__init__':
     os.system('cls && title [The TikTok Bot]')
     main = TikTok()
